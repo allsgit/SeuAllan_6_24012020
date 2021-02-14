cfc4a8ea17cddf32b7ff4c0a160a4c07f5e0b638
alls <allanseu@gmail.com>
2021-02-14 13:45:53 +0100

    photographer page done 90%, il faut ajouter les prix correspondand à chaque vignette

---
+++ modal.js
@@ -0,0 +1,6 @@
+
+let closeCross = document.getElementById('close');
+let modal = document.getElementById('modal');
+closeCross.addEventListener('click', function(){
+modal.style.display ="none";
+})
