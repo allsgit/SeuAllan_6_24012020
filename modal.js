6aef710277dc09814ba401bc317ff8b9ac28f694
alls <allanseu@gmail.com>
2021-03-20 13:28:27 +0100

    test

--- modal.js
+++
@@ -1,6 +0,0 @@
-
-let closeCross = document.getElementById('close');
-let modal = document.getElementById('modal');
-closeCross.addEventListener('click', function(){
-modal.style.display ="none";
-})
