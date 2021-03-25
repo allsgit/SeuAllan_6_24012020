c3204830214c3e4cd8cda3652128c8b3be233d0b
alls <allanseu@gmail.com>
2021-03-25 12:03:49 +0100

    avancement sort

--- JS/photographer-page.js
+++ JS/photographer-page.js
@@ -1,128 +1,133 @@
 const Data = fetch ('http://127.0.0.1:5501/JS/photograph-list.json')
   .then (response => response.json ())
   .then (data => {
+    testoncela (data.media, data.photographers);
+  });
+function testoncela (media, photographers) {
+  photographers.map (element => {
+    if (element.id === 243) {
+      const headerProfil = document.querySelector ('.photographer-header');
+      const profile = document.querySelector ('.photographer-profile');
+      const photographerName = document.createElement ('h1');
+      const photographerLocation = document.createElement ('p');
+      const photographerTagLine = document.createElement ('p');
+      const photographerPrice = document.createElement ('p');
+      const IdPhoto = document.createElement ('div');
 
-    const headerProfil = document.querySelector ('.photographer-header');
-    const profile = document.querySelector ('.photographer-profile');
-    const photographerName = document.createElement ('h1');
-    const photographerLocation = document.createElement ('p');
-    const photographerTagLine = document.createElement ('p');
-    const photographerPrice = document.createElement ('p');
-    const IdPhoto = document.createElement ('div');
+      const IdPhotoImg = document.createElement ('img');
+      IdPhotoImg.src = `/image/Photographers ID Photos/${element.portrait}`;
 
-    const IdPhotoImg = document.createElement ('img');
-    IdPhotoImg.src = `/image/Photographers ID Photos/${data.photographers[0].portrait}`;
+      console.log (photographers.portrait);
 
-    IdPhoto.classList = 'id-picture';
-    photographerName.classList = 'photographer-name';
-    photographerLocation.classList = 'photographer-location-profile';
-    photographerTagLine.classList = 'photographer-tag-profile';
-    photographerPrice.classList = 'photographer-price-profile';
+      IdPhoto.classList = 'id-picture';
+      photographerName.classList = 'photographer-name';
+      photographerLocation.classList = 'photographer-location-profile';
+      photographerTagLine.classList = 'photographer-tag-profile';
+      photographerPrice.classList = 'photographer-price-profile';
 
-    headerProfil.appendChild (IdPhoto);
-    IdPhoto.appendChild (IdPhotoImg);
-    profile.appendChild (photographerName);
-    profile.appendChild (photographerLocation);
-    profile.appendChild (photographerPrice);
-    profile.appendChild (photographerTagLine);
+      headerProfil.appendChild (IdPhoto);
+      IdPhoto.appendChild (IdPhotoImg);
+      profile.appendChild (photographerName);
+      profile.appendChild (photographerLocation);
+      profile.appendChild (photographerPrice);
+      profile.appendChild (photographerTagLine);
 
-    photographerName.innerText = data.photographers[0].name;
-    photographerTagLine.innerText = data.photographers[0].tagline;
-    photographerLocation.innerText = `${data.photographers[0].city} , ${data.photographers[0].country}`;
-    photographerPrice.innerText = `${data.photographers[0].price} €/ jours`;
-
-    const likeResume = document.querySelector ('.resume_like_price');
-    const LikeCounterFull = document.createElement ('p');
-    LikeCounterFull.classList = 'total_counter_resume';
-    likeResume.appendChild(LikeCounterFull);
-    LikeCounterFull.innerText = "000";
-
-    const heartBlack = document.createElement ('img')
-    heartBlack.classList = 'heart-icon-resume';
-    likeResume.appendChild(heartBlack);
-    heartBlack.src = "/image/heart-black.png"
-    
-    const PricePerDay = document.createElement ('p')
-    PricePerDay.classList = 'price_resume';
-    likeResume.appendChild(PricePerDay);
-    PricePerDay.innerText = "300€ /jours"
-
-    for (let j = 0; j < data.media.length; j++){
-      if (data.media[j].photographerId === 243){
-        const photographerPic = document.createElement ('img');
-        const thumbSection = document.querySelector ('.thumb-section');
-        const galleryPic = document.createElement ('div');
-        galleryPic.classList = 'thumb-img';
-        thumbSection.appendChild (galleryPic);
-        photographerPic.src = `/image/Mimi/${data.media[j].image} `;
-        galleryPic.appendChild (photographerPic);
-      }
+      photographerName.innerText = element.name;
+      photographerTagLine.innerText = element.tagline;
+      photographerLocation.innerText = `${element.city} , ${element.country}`;
+      photographerPrice.innerText = `${element.price} €/ jours`;
     }
-   
-// boucle pour afficher  gallery des photographes 
-    /*for (let j = 0; j < data.media.length; j++) {
-      if (data.media[j].photographerId === 243) {
-        const photographerPic = document.createElement ('img');
+  });
 
-        const thumbSection = document.querySelector ('.thumb-section');
-        const galleryPic = document.createElement ('div');
-        galleryPic.classList = 'thumb-img';
-        thumbSection.appendChild (galleryPic);
+  let likeArray = [];
+  const likeResume = document.querySelector ('.resume_like_price');
+  const LikeCounterFull = document.createElement ('p');
+  LikeCounterFull.classList = 'total_counter_resume';
+  likeResume.appendChild (LikeCounterFull);
+  LikeCounterFull.innerText = likeArray;
 
-        galleryPic.appendChild (photographerPic);
-        photographerPic.src = `/image/Mimi/${data.media[j].image} `;
+  const heartBlack = document.createElement ('img');
+  heartBlack.classList = 'heart-icon-resume';
+  likeResume.appendChild (heartBlack);
+  heartBlack.src = '/image/heart-black.png';
 
-        const picName = document.createElement ('p');
-        picName.classList = 'picture_name';
-        galleryPic.appendChild (picName);
-        picName.innerText = `${data.media[j].image}`;
+  const PricePerDay = document.createElement ('p');
+  PricePerDay.classList = 'price_resume';
+  likeResume.appendChild (PricePerDay);
+  PricePerDay.innerText = '300€ /jours';
 
-        const picPrice = document.createElement ('p');
-        picPrice.classList = 'picture_price';
-        galleryPic.appendChild (picPrice);
-        picPrice.innerText = `${data.media[j].price}€`;
+  const sorted = media.sort ((a, b) => b.likes - a.likes).map (element => {
+ 
+    if (element.photographerId === 243) {
+      const sortIcon = document.querySelector ('.up_arrow_icon')
+      sortIcon.addEventListener('click',() => {
+      
+      })
+      console.log(element.likes);
+      const photographerPic = document.createElement ('img');
+      const thumbSection = document.querySelector ('.thumb-section');
+      const galleryPic = document.createElement ('div');
+      galleryPic.classList = 'thumb-img';
+      thumbSection.appendChild (galleryPic);
+      galleryPic.appendChild (photographerPic);
+      photographerPic.src = `/image/Mimi/${element.image} `;
+
+      const picName = document.createElement ('p');
+      picName.classList = 'picture_name';
+      galleryPic.appendChild (picName);
+      picName.innerText = `${element.image}`;
+
+      const picPrice = document.createElement ('p');
+      picPrice.classList = 'picture_price';
+      galleryPic.appendChild (picPrice);
+      picPrice.innerText = `${element.price}€`;
+
+      const heartIcon = document.createElement ('img');
+      heartIcon.classList = 'heart-icon';
+      galleryPic.appendChild (heartIcon);
+      heartIcon.src = '/image/heart.png';
+
+      const likeCounter = document.createElement ('p');
+      likeCounter.classList = 'like_counter';
+      galleryPic.appendChild (likeCounter);
+      let test = (likeCounter.innerText = element.likes);
+
+      //
+      // INCREMENT EACH LIKE + TOTAL LIKE
+
+      const inputValue = element.likes;
+      likeArray.push (inputValue); // likeArray est un tableau vide
+      const reducer = (acc, curr) => acc + curr;
+      let sumLike = likeArray.reduce (reducer);
+      tatatat = LikeCounterFull.innerText = sumLike;
+      console.log (sumLike);
+
+      heartIcon.addEventListener ('click', $event => {
+        $event.stopImmediatePropagation ();
+        console.log ($event);
+        LikeCounterFull.innerText = tatatat++;
+        console.log (element.likes);
+        element.likes++;
+        const like = element.likes;
+
+        likeCounter.innerText = like; //element.likes++;
+        console.log (element.likes);
+      });
+    }
+  });
+
+}
 
 
-
-///////
-
-        const heartIcon = document.createElement ('img');
-        heartIcon.classList = 'heart-icon';
-        galleryPic.appendChild (heartIcon);
-        heartIcon.src = '/image/heart.png';
-
-        const likeCounter = document.createElement ('p');
-        likeCounter.classList = 'like_counter';
-        galleryPic.appendChild (likeCounter);
-
-        likeCounter.innerText = data.media[j].likes;
-        const testb = document.querySelector ('.heart-icon');
-        let haha = data.media[j].likes;
-        console.log(haha);
-        
-        testb.addEventListener ('click', function (event) {
-          let buttonClicked = event.target
-          if (buttonClicked) {
-            haha++;
-            console.log (haha);
-            likeCounter.innerText = haha;
-          }
-          
-        
-        });
-      }
-    };*/
 
 // afficher les tags sur page perso photographe //
-const showOwnTags = function (element){
-      const tagButton = document.createElement ('button');
-      tagButton.classList = 'searchButton';
-      tagButton.innerText = (element);
-      profile.appendChild (tagButton);
-}
-const mimiKeelTag = data.photographers[0].tags.map(element => element)
-showOwnTags(mimiKeelTag);
-
-
-
-  });
+const showOwnTags = function (TagElement) {
+  const tagButton = document.createElement ('button');
+  tagButton.classList = 'searchButton';
+  tagButton.innerText = TagElement;
+  profile.appendChild (tagButton);
+};
+/*
+const mimiKeelTag = photographers[0].tags.map((element) => {
+  showOwnTags(element);
+})*/
